768aebe8728b1f4461d5de0264c121f68165d8e9
Petri Lehtinen <petri@digip.org>
2023-11-12 21:56:35 +0200

    XON/XOFF flow control

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,9 @@ use microbit::Peripherals;
 const TX_PIN: u32 = 2;
 const RX_PIN: u32 = 3;
 
+const XON: u8 = 0x11;
+const XOFF: u8 = 0x13;
+
 #[entry]
 fn main() -> ! {
     let p = Peripherals::take().unwrap();
@@ -40,12 +43,22 @@ fn main() -> ! {
     radio.init(&p.CLOCK);
 
     let mut uart_to_radio: Queue<u8, 1024> = Queue::new();
+    let mut uart_to_radio_control = Control::new();
+
     let mut radio_to_uart: Queue<u8, 1024> = Queue::new();
+    let mut radio_to_uart_control = Control::new();
 
     loop {
         let now = rtc.tick();
         uart.tick(now, &mut radio_to_uart, &mut uart_to_radio);
         radio.tick(now, &mut uart_to_radio, &mut radio_to_uart);
+
+        if let Some(c) = radio_to_uart_control.control(&radio_to_uart) {
+            uart_to_radio.enqueue(c).unwrap();
+        }
+        if let Some(c) = uart_to_radio_control.control(&uart_to_radio) {
+            radio_to_uart.enqueue(c).unwrap();
+        }
     }
 }
 
@@ -55,5 +68,27 @@ fn panic(_info: &PanicInfo) -> ! {
     debug!("panic");
     loop {
         atomic::compiler_fence(Ordering::SeqCst);
+    }
+}
+
+struct Control {
+    xoff: bool,
+}
+
+impl Control {
+    fn new() -> Self {
+        Self { xoff: false }
+    }
+
+    fn control(&mut self, queue: &Queue<u8, 1024>) -> Option<u8> {
+        if queue.len() > 512 && !self.xoff {
+            self.xoff = true;
+            Some(XOFF)
+        } else if queue.len() < 256 && self.xoff {
+            self.xoff = false;
+            Some(XON)
+        } else {
+            None
+        }
     }
 }
